22e08d36d56227011f84e82b5867690235a42120
ntsang26 <ntsthanhsangbn@gmail.com>
2024-05-05 15:31:58 +0700

    update

---
+++ config/database.js
@@ -0,0 +1,25 @@
+
+//module.exports = {
+//	'url': 'mongodb://127.0.0.1:27017',
+//	'options': {
+//		'user':   'admin',
+//		'pass':   'YouPassforUser',
+//		'dbName': 'PlayGameChuan2',
+//		'useNewUrlParser': true,
+//		'useUnifiedTopology': true,
+//		//'autoIndex':       false,
+//	},
+//};
+
+
+module.exports = {
+	'url': 'mongodb://localhost:27017',
+	'options': {
+	//	'user':   'admin',
+	//	'pass':   'Ddata@123456',
+		'dbName': 'PlayGame', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
